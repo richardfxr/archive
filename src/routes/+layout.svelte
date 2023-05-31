82764ca04bb972e5f04f23c51610dad01d628b9d
Richard Fu <richardfxr.personal@gmail.com>
2023-05-30 21:30:12 -0400

    🟠SVLT: move .wrapper & header to index page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,5 @@
-<script lang="ts">
-    /* === IMPORTS ============================ */
-    import Header from "$lib/header.svelte";
-</script>
-
 <div class="fullWidth">
-    <div class="wrapper">
-        <Header />
-
-        <div class="content">
-            <main id="main">
-                <slot></slot>
-            </main>
-        </div>
-    </div>
+    <slot></slot>
 </div>
 
 
@@ -33,78 +20,6 @@
             height: $border-width;
     
             background-color: var(--clr-border-bg);
-        }
-    }
-
-    .wrapper {
-        max-width: var(--maxWidth);
-
-        border-right: $border var(--clr-border-bg);
-        border-left: $border var(--clr-border-bg);
-        margin: 0 auto;
-    }
-
-    .content {
-        min-height: 100vh;
-
-        padding: var(--pad-border);
-
-        #main {
-            height: 200vh;
-        }
-    }
-
-
-
-    // === BREAKPOINTS ========================
-    @media (min-width: $breakpoint-stacked) {
-        .wrapper {
-            display: grid;
-            grid-template-rows: auto 1fr;
-            grid-template-areas:
-                "header content"
-                "filters content";
-            align-items: start;
-            column-gap: var(--pad-xl);
-        }
-
-        :global(.header) {
-            grid-area: header;
-        }
-
-        :global(.searchForm) {
-            grid-area: filters;
-        }
-
-        .content {
-            grid-area: content;
-
-            padding-left: calc(var(--pad-border) * 0.5);
-            border-left: $border var(--clr-border-bg);
-        }
-    }
-
-    @media (max-width: $breakpoint-stacked) {
-        .content {
-            padding-left: calc(var(--pad-border) * 0.5);
-        }
-    }
-
-    @media (min-width: $breakpoint-4col) {
-        .wrapper {
-            grid-template-columns: 1fr 4fr;
-        }
-    }
-
-    @media (max-width: $breakpoint-4col) {
-        .wrapper {
-            grid-template-columns: 1fr 3fr;
-        }
-    }
-
-    @media (max-width: $breakpoint-3col) {
-        .wrapper {
-            grid-template-columns: 1fr 2fr;
         }
     }
 </style>
